24d5dc7fc7d41f68a0226a3c7e877dac6511a96d
ItaloAraujoGomes <italoaraujogomes@gmail.com>
2026-03-04 10:34:51 -0300

    Criando algumas validações de produtos.

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -7,5 +7,9 @@ export function errorHandling(error: any, request: Request, response: Response, 
         return response.status(error.statusCode).json({ message: error.message})
     }
 
+    if( error instanceof ZodError){
+        return response.status(400).json({ message: "validation error", issues: error.format() })
+    }
+
     return response.status(500).json({ message: error.message })
 }
